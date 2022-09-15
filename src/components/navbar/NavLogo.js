4277e66776e49154a35a60544020730ffc86df5d
Alaz Tetik <alaztetik@gmail.com>
2022-09-15 21:42:40 +0300

    Add firm logo some margin

--- src/components/navbar/NavLogo.js
+++ src/components/navbar/NavLogo.js
@@ -4,7 +4,7 @@ export default function NavLogo(props) {
 
     return (
         <Link to="/dashboard" className="my-auto mx-3">
-            <img src={props.imageSource} className="w-14"/>
+            <img src={props.imageSource} className="w-14 m-1" alt="Orlando Language Logo" />
         </Link>
     );
 }
